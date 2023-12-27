fc2430d93c4c27710d0eeaad7d2baea508c5230c
RAABIN SINGH  S A <112077212+raabin10@users.noreply.github.com>
2023-12-27 08:44:45 +0000

    code done

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,34 @@
+import React, { Component } from 'react';
 
-import React from "react";
-import './../styles/App.css';
+class App extends Component {
+ constructor(props) {
+    super(props);
+    this.state = {
+      weather: { temperature: 25, conditions: "Sunny" },
+      color: ''
+    };
+ }
 
-const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+ componentDidMount() {
+    this.updateColor();
+ }
+
+ updateColor() {
+    const { weather } = this.state;
+    const color = weather.temperature > 20 ? 'red' : 'blue';
+    this.setState({ color });
+ }
+
+ render() {
+    const { weather, color } = this.state;
+    return (
+      <div>
+        <h1>Current Weather</h1>
+        <p>Temperature: <span style={{ color }}>{weather.temperature}</span> degrees</p>
+        <p>Conditions: {weather.conditions}</p>
+      </div>
+    );
+ }
 }
 
-export default App
+export default App;
